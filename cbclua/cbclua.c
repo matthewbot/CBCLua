8b011e9521e4ded45147e25cfaba7ecd9b402ec3
Matthew Thompson <matthewbot@gmail.com>
2009-04-18 18:29:47 +0000

    Various bugfixes, now extracts tar properly

--- cbclua/cbclua.c
+++ cbclua/cbclua.c
@@ -2,13 +2,13 @@
 // It allows users to run cbclua through the cbcui
 
 const char *errmsg = "\
-Unable to launch cbclua, error %d.\n\
-You should probably do a full\n\
-firmware reinstall.\
+Unable to launch CBCLua, error %d.\n\
+You probably need to reinstall\
+CBCLua through its userhook0\
 ";
 
 int main(int argc, char **argv) {
-	int status = system("/mnt/user/cbclua/run.sh");
+	int status = system("/mnt/user/code/cbclua/run.sh");
 
 	if (status != 0) {
 		fprintf(stderr, errmsg, status);

9eedfaa88e5364eca2b38daf1b731f375d7769aa
Matthew Thompson <matthewbot@gmail.com>
2009-04-28 01:36:30 +0000

    Small tweaks to wifi install

--- wifiinstall.sh
+++ wifiinstall.sh
@@ -1,7 +1,7 @@
-#!/bin/sh -x
+#!/bin/sh
 
 echo -n "CBC IP Address: "
 read IP
 
-tar -czf - cbclua/ -h -p | ssh root@${IP} "rm -rf /mnt/user/code/cbclua; tar -C /mnt/user/code -zxf -" -p
+tar -czf - cbclua/ -h -p | ssh root@${IP} "rm -rf /mnt/user/code/cbclua; tar -C /mnt/user/code -zxf - -p"
 

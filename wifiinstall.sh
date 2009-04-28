9462d8ca88f99d003ff0cd67fba97259e05d78f3
Matthew Thompson <matthewbot@gmail.com>
2009-04-28 01:57:07 +0000

    Added in exclusions for wifi install

--- wifiinstall.sh
+++ wifiinstall.sh
@@ -3,5 +3,5 @@
 echo -n "CBC IP Address: "
 read IP
 
-tar -czf - cbclua/ -h -p | ssh root@${IP} "rm -rf /mnt/user/code/cbclua; tar -C /mnt/user/code -zxf - -p"
+tar -czf - cbclua/ --exclude=".*" --exclude="*~" -h -p | ssh root@${IP} "rm -rf /mnt/user/code/cbclua; tar -C /mnt/user/code -zxf - -p"
 

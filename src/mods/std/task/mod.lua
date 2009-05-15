783f05abf258fe7a2a20317cc3c3765738fe0dd7
Matthew Thompson <matthewbot@gmail.com>
2009-05-15 23:15:59 +0000

    Forgot to make signal a submodule of task

--- src/mods/std/task/mod.lua
+++ src/mods/std/task/mod.lua
@@ -4,5 +4,6 @@ local task = require "std.task.task"
 
 submodule("std.task.control")
 submodule("std.task.list")
+submodule("std.task.signal")
 get_current = task.get_current -- export one function from task
 

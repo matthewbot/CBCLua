848a888869355ea946285c44ebd997f861ebb652
Matthew Thompson <matthewbot@gmail.com>
2009-05-18 02:04:08 +0000

    Added helper method to find local variables by name

--- src/startup/util.lua
+++ src/startup/util.lua
@@ -9,3 +9,17 @@ end
 function io.writeln(...)
 	return io.write(..., "\n")
 end
+
+function debug.findlocal(level, name)
+	level = level+1
+	local num=1
+	while true do
+		local localname, localval = debug.getlocal(level, num)
+		if localname == name then
+			return localval
+		elseif localname == nil then
+			return nil
+		end
+		num = num+1
+	end
+end

e23a58dc318a828598cba442f41555d0f7f43d09
Matthew Thompson <matthewbot@gmail.com>
2009-05-18 02:03:45 +0000

    Added a super class accessor table

--- src/startup/class.lua
+++ src/startup/class.lua
@@ -47,18 +47,24 @@ function class_mt.__index(class, key)
 	return class.methods[key]
 end
 
-local function find_method(class, methname)
-	local meth = class.methods[methname]
-	if meth then
-		return meth
-	end
-	
+local find_method
+
+local function find_method_mixins(class, methname)
 	for _,mixinclass in ipairs(class.mixins) do
 		meth = find_method(mixinclass, methname)
 		if meth then
 			return meth
 		end
 	end
+end
+
+function find_method(class, methname)
+	local meth = class.methods[methname]
+	if meth then
+		return meth
+	end
+	
+	return find_method_mixins(class, methname)
 end
 
 function inst_mt.__index(inst, key)
@@ -99,3 +105,27 @@ for _,mmname in pairs(mmnames) do
 	mmname = "__" .. mmname
 	inst_mt[mmname] = make_metamethod(mmname)
 end
+
+-- Make a super table
+
+super = { }
+local super_mt = { }
+setmetatable(super, super_mt)
+
+function super_mt.__index(super, methname)
+	local self = debug.findlocal(2, "self")
+	if self == nil then
+		error("Cannot refer to super outside of a method!", 2)
+	end
+	
+	local class = self.class
+	local meth = find_method_mixins(class, methname)
+	if meth == nil then
+		error("No superclass of class " .. class.name .. " contains a method named " .. methname)
+	end
+	
+	return function (super, ...) -- return a thunk closure
+		return meth(self, ...) -- that replaces the 'self' arg from the useless super table to the actual self object
+	end
+end
+	

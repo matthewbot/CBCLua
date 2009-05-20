27ec8a01e60a86e713dd9a9f2e9a0d921dad4039
Matthew Thompson <matthewbot@gmail.com>
2009-05-20 00:33:16 +0000

    Wrote new robot.c based installer

---
+++ usbinstall/robot.c
@@ -0,0 +1,12 @@
+int main() {
+	printf("Installing CBCLua v0.5!\n");
+	
+	system("mkdir -p /mnt/tmpusb");
+	system("mount /dev/sdb1 /mnt/tmpusb -t vfat -o ro");
+	system("rm -rf /mnt/user/code/cbclua");
+	system("tar -xzf /mnt/tmpusb/cbclua.tgz -C /mnt/user/code -p");
+	system("umount /mnt/tmpusb");
+	system("sync");
+	
+	printf("CBCLua installed!\n");
+}

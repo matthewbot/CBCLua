80f0b2de9c19f9220eff8ba66e161759be0bc8ec
Matthew Thompson <matthewbot@gmail.com>
2009-05-21 01:24:49 +0000

    Added auto detect mechanism for standard lua pkg-config or debian's packaging lua5.1

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@
 # your system's lua.h and friends are located
 # (Or adjust the pkg-config if lua is installed as something other than lua5.1)
 
-CONF_LOCAL_LUA_CFLAGS := $(shell pkg-config lua5.1 --cflags)
+CONF_LOCAL_LUA_CFLAGS := $(shell pkg-config lua --cflags --exists 2>/dev/null || pkg-config lua5.1 --cflags --exists 2>/dev/null)

07c19778248ab246397688794b7880cd5878cdf7
Matthew Thompson <matthewbot@gmail.com>
2010-01-06 04:53:54 +0000

    Fixed to properly invoke pkg-config

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # Change the below value to set the clfags to includethe directory where 
 # your system's lua.h and friends are located
-# (Or adjust the pkg-config if lua is installed as something other than lua5.1)
+# (Or adjust the pkg-config if lua is installed as something other than lua or lua5.1)
 
-CONF_LOCAL_LUA_CFLAGS := $(shell pkg-config lua --cflags --exists 2>/dev/null || pkg-config lua5.1 --cflags --exists 2>/dev/null)
+CONF_LOCAL_LUA_CFLAGS := $(shell pkg-config lua --cflags 2>/dev/null || pkg-config lua5.1 --cflags 2>/dev/null)

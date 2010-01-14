4b07145793c9feca3daa03104fd76a6c230200cc
Matthew Thompson <matthewbot@gmail.com>
2010-01-14 01:46:08 +0000

    New less invasive module system that enables special cbclua features only within certain directories given by environment variables from run.sh

--- src/startup/module.lua
+++ src/startup/module.lua
@@ -1,76 +1,66 @@
--- Some expansions on the module system
+-- CBCLua Module system
+
+-- Public API for CBCLua Modules --
 
 local mod_mt = { }
+local get_system_table
 
--- Module modifier that allows import() / export() to work. Also allows access to global functions
+-- Automatically require packages beginning with the prefix
 
-function cbcluamodule(mod)
-	if mod._CBCLUAMODULE then return end
-	
-	mod._M = nil
-	mod._IMPORTS = { }
-	mod._EXPORTS = { }
-	mod._AUTOREQS = { }
-	mod._CBCLUAMODULE = true
-	setmetatable(mod, mod_mt)
-end
+function autorequire(name, mod)
+	mod = mod or getfenv(2)	
+	assert(getmetatable(mod) == mod_mt, "import may only be called from within a module!")
 
--- Automatically require packages beginning with the prefix (defaults to std)
-
-function autorequire(arg, mod)
-	if mod == nil and type(arg) == "string" then -- just given a string
-		return function (mod) -- make a closure for module() to call
-			cbcluamodule(mod)
-			table.insert(mod._AUTOREQS, arg)
-		end
-	else
-		cbcluamodule(mod)
-		table.insert(mod._AUTOREQS, arg)
-	end
+	local autoreqs = get_system_table(mod, "_AUTOREQS")
+	table.insert(autoreqs, name)
 end
 
 -- This function loads a module, then makes it visible in the current module
 -- from the outside however, the imported module is not visible
 
 function import(name, mod)
-	local outermod = mod or getfenv(2)	
-	assert(outermod._NAME, "import may only be called from within a module!")
+	mod = mod or getfenv(2)	
+	assert(getmetatable(mod) == mod_mt, "import may only be called from within a module!")
 
-	table.insert(outermod._IMPORTS, require(name))
+	local imports = get_system_table(mod, "_IMPORTS")
+	table.insert(imports, require(name))
 end
 
 -- This function makes a module part of the current module's public interface
 
 function export(name, mod)
-	local outermod = mod or getfenv(2)
-	assert(outermod._NAME, "export may only be called from within a module!")
+	mod = mod or getfenv(2)
+	assert(getmetatable(mod) == mod_mt, "export may only be called from within a module!")
 	
-	table.insert(outermod._EXPORTS, require(name))
+	local exports = get_system_table(mod, "_EXPORTS")
+	table.insert(exports, require(name))
 end
+	
+-- Internals
 
--- This function replaces module() and makes it so that leaving off any module modifiers automatically
--- adds the cbcluamodule modifier. Our changes shouldn't break any code operating under the expectations
--- of the real module function, since everything we don't alter any behavior, just add new ones
-
-local realmodule = module
-local _G = _G
-function module(name, ...)
-	if _G.select("#", ...) == 0 and not name:find("^socket") then 
-		realmodule(name, _G.cbcluamodule)
-	else
-		realmodule(name, ...)
+function get_system_table(mod, tabname)
+	local tab = mod[tabname]
+	if tab == nil then
+		tab = { }
+		mod[tabname] = tab
 	end
 	
-	_G.setfenv(2, _G.getfenv(1)) -- propogate environment set by realmodule to caller
+	return tab
 end
-
--- This function is a metatable function that allows modules to view global functions and imported modules
+	
+local function dummymodule()
+end
+	
+-- Module metatable
 
 function mod_mt.__index(mod, key)
-	for _,exportmod in ipairs(mod._EXPORTS) do -- look in export modules
-		local val = exportmod[key]
-		if val then
-			return val
+	local exports = rawget(mod, "_EXPORTS")
+	if exports then
+		for _,exportmod in ipairs(exports) do -- look in export modules
+			local val = exportmod[key]
+			if val then
+				return val
+			end
 		end
 	end
 
@@ -90,85 +80,71 @@ function mod_mt.__index(mod, key)
 		return super
 	end
 	
+	if key == 'module' then
+		return dummymodule
+	end
+	
 	local val = _G[key] -- otherwise, go through global functions
 	if type(val) == "function" then
 		return val
 	end	
 	
-	for _,importmod in ipairs(mod._IMPORTS) do -- look through its imports to find the value
-		local val = importmod[key]
-		if val then
-			return val
-		end
-	end
-	
-	for _,autoreqprefix in ipairs(mod._AUTOREQS) do
-		local modname = autoreqprefix .. key
-		local ok, automod = pcall(require, modname)
-		if ok then
-			return automod
-		end
-	end
-end
-
--- This allows assignments into exported modules and imports
--- it also hacks around lua's module system's behavior of adding tables for submodules in a module table
-
-function mod_mt.__newindex(mod, key, value)	
-	if type(value) == "table" and getfenv(2) == _G then -- if its a table being assigned in, and the caller's environment is global
-		return -- ignore it, lua's module system is creating globals inside our tables
-	end
-
-	for _,exportmod in ipairs(mod._EXPORTS) do -- look in exported modules
-		if exportmod[key] then -- if it exists in the exported module (this will not allow assignment to nil variables!)
-			rawset(exportmod, key, value) -- set it
-			return
-		end
-	end
-	
-	if getfenv(2) ~= mod then -- if the caller was declared inside the module
-		for _,importmod in ipairs(mod._IMPORTS) do -- look through its imports to find the value
-			if importmod[key] then
-				rawset(importmod, key, value)
-				return
+	local imports = rawget(mod, "_IMPORTS")
+	if imports then
+		for _,importmod in ipairs(imports) do -- look through its imports to find the value
+			local val = importmod[key]
+			if val then
+				return val
 			end
 		end
 	end
 	
-	rawset(mod, key, value)
+	local autoreqs = rawget(mod, "_AUTOREQS")
+	if autoreqs then
+		for _,autoreqprefix in ipairs(autoreqs) do -- figure out if we can autorequire it
+			local modname = autoreqprefix .. key
+			local ok, automod = pcall(require, modname)
+			if ok then
+				return automod
+			end
+		end
+	end
 end
 
--- This function is used to do delayed module loading
--- original use was in the startup code, log and task have dependencies on each other
--- since log wants to display the current task and task wants to log messages
--- log uses a require_later on task to break the cyclic dependency
-
-local mock_mod_mt = { }
-
-function require_later(name)
-	local mock_mod = { name = name }
-	setmetatable(mock_mod, mock_mod_mt)
-	return mock_mod
-end
-
-local function get_mod(mock_mod)
-	local mod = rawget(mock_mod, mod)
+local CBCLUA_CODEPATH = os.getenv("CBCLUA_CODEPATH")
+local CBCLUA_MODPATH = os.getenv("CBCLUA_MODPATH")
 	
-	if mod == nil then
-		mod = require(mock_mod.name)
-		rawset(mock_mod, "mod", mod)
+local function tryload(fname)
+	local f = io.open(fname)
+	if f then
+		f:close()
+		return loadfile(fname)
+	end
+end
+	
+local function loader(name)
+	local pathname = name:gsub("%.", "/")
+	filename = pathname .. ".lua"
+	modfilename = pathname .. "/mod.lua"
+	
+	local modfunc = tryload(CBCLUA_CODEPATH .. filename) or 
+	                tryload(CBCLUA_CODEPATH .. modfilename) or
+	                tryload(CBCLUA_MODPATH .. filename) or
+	                tryload(CBCLUA_MODPATH .. modfilename)
+	if modfunc == nil then
+		return nil
 	end
 	
-	return mod
-end
-
-function mock_mod_mt.__index(mock_mod, key)
-	local mod = get_mod(mock_mod)
-	return mod[key]
-end
-
-function mock_mod_mt.__newindex(mock_mod, key, val)
-	local mod = get_mod(mock_mod)
-	mod[key] = val
-end
+	local mod = { _NAME = name }
 	
+	setmetatable(mod, mod_mt)
+	setfenv(modfunc, mod)
+	
+	return function ()
+		package.loaded[name] = mod -- this, for some reason, must happen inside the module function
+		return modfunc() -- so we make a thunk wrapper around it
+	end
+end
+
+table.insert(package.loaders, 1, loader)
+

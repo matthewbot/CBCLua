6dbd003befe996fb0108b2d915660f77171f0bc4
Matthew Thompson <matthewbot@gmail.com>
2010-03-11 00:11:19 -0500

    Version bump to 0.95

--- src/startup/config.lua
+++ src/startup/config.lua
@@ -1,4 +1,4 @@
-local version = "CBCLua 2 v0.9"
+local version = "CBCLua 2 v0.95"
 local codepath = os.getenv("CBCLUA_CODEPATH")
 local modpath = os.getenv("CBCLUA_MODSPATH")
 local name_filename = os.getenv("CBCLUA_NAME_FILE")

2fe41d2bb1b15beb28fd86464360922aad9ee2ab
Matthew Thompson <matthewbot@gmail.com>
2010-03-20 13:48:20 -0400

    Wrote timerlist module

--- src/mods/sched/timerlist.lua
+++ src/mods/sched/timerlist.lua
@@ -2,6 +2,30 @@ module("cbclua.sched.timerlist")
 local timer = require "cbclua.timer"
 local table = require "table"
 
+--[[ TimerEntry ]]--
+
+TimerEntry = create_class "TimerEntry"
+
+function TimerEntry:construct(time, func)
+	self.time = time
+	self.func = func
+end
+
+function TimerEntry:get_time()
+	return self.time
+end
+
+function TimerEntry:get_time_remaining(curtime)
+	curtime = curtime or timer.seconds()
+	return self.time - curtime
+end
+
+function TimerEntry:run()
+	return self.func()
+end
+
+--[[ timerlist ]]--
+
 local timerlist = { }
 
 function register_timer(timer)
@@ -15,9 +39,29 @@ function register_timer(timer)
 	table.insert(timerlist, pos, timer)
 end
 
-function cancel_timer(timer)
+function unregister_timer(timer)
 	local pos = table.findvalue(timerlist, timer)
+	if not pos then return end
+	
 	table.remove(timerlist, pos)
+end
+
+function new_timer(...)
+	local timer = TimerEntry(...)
+	register_timer(timer)
+	return timer
+end
+
+--[[ main funcs ]]--
+
+function get_count()
+	return #timerlist
+end
+
+function get_next_time()
+	if #timerlist >= 1 then
+		return timerlist[1]:get_time()
+	end
 end
 
 function run()

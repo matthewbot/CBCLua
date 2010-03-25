8f313903f6c8a2ce098901c6ecc39bfca6d77895
Matthew Thompson <matthewbot@gmail.com>
2010-03-24 21:19:10 -0400

    Wrote iolist module

---
+++ src/mods/sched/iolist.lua
@@ -0,0 +1,66 @@
+module("cbclua.sched.iolist")
+local timer = require "cbclua.timer"
+local table = require "table"
+local math = require "math"
+local list = require "list"
+
+--[[ IOEntry ]]--
+
+IOEntry = create_class "IOEntry"
+
+function IOEntry:construct(file, func)
+	self.file = file
+	self.func = func
+end
+
+function IOEntry:get_file()
+	return self.file
+end
+
+function IOEntry:run()
+	return self.func()
+end
+
+--[[ iolist ]]--
+
+local iolist = list.new{}
+
+function register_ioentry(ioentry)
+	table.insert(iolist, ioentry)
+end
+
+function unregister_ioentry(ioentry)
+	local pos = table.findvalue(iolist, ioentry)
+	if not pos then return end
+	table.remove(iolist, pos)
+end
+
+function new_ioentry(...)
+	local ioentry = IOEntry(...)
+	register_ioentry(ioentry)
+	return ioentry
+end
+
+--[[ main funcs ]]--
+
+function block(sleepamt)
+	if sleepamt == math.inf then
+		sleepamt = -1
+	end
+
+	local files = list.map(function (ioentry) return ioentry:get_file() end, iolist)
+	local flags = { timer.sleep_select(sleepamt, unpack(files)) }
+	
+	local runentries = { }
+	for i, entry in ipairs(iolist) do
+		if flags[i] then
+			table.insert(runentries, entry)
+		end
+	end
+	
+	for entry in list.elems(runentries) do
+		unregister_ioentry(entry)
+		entry:run()
+	end
+end
+
